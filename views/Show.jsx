45d6324c331674a0c6e2740332911c5fa60b8636
Caleb Krysos <CalMc22>
2024-01-23 00:36:04 -0500

    fixed images not showing

--- views/Show.jsx
+++ views/Show.jsx
@@ -6,7 +6,7 @@ const Show = ({ pokemon }) => {
     <div style={{ textAlign: 'center' }}>
       <h1>Gotta Catch 'Em All</h1>
       <h2>{pokemon.name}</h2>
-      <img src={`${pokemon.img}.jpg`} alt={pokemon.name} style={{ maxWidth: '300px', marginBottom: '20px' }} />
+      <img src={`${pokemon.img}`} alt={pokemon.name} style={{ maxWidth: '300px', marginBottom: '20px' }} />
       <a href="/pokemon" style={{ textDecoration: 'none', color: '#007bff', fontSize: '18px' }}>
         Back
       </a>
